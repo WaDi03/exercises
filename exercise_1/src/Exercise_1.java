1fb198b489cfa7abade329251bdf053f139250ba
wahid riazi <wriazi@terrestris.de>
2023-07-13 16:14:05 +0200

    new exercise hash

---
+++ exercise_1/src/Exercise_1.java
@@ -0,0 +1,40 @@
+    class Frage1{
+        String A$B;
+        String _helloworld;
+        String  true;
+        String java.lang;
+        String Public;
+        String 1980_s;
+    }
+    class Frage2{
+    private String brand;
+    private boolean empty;
+
+    public static void main (String[] args){
+        Frage2 wb = new Frage2();
+        System.out.println("Empty 0" +wb.empty);
+        System.out.println(", Brand = " +wb.brand);
+    }
+    }
+    class Frage3{
+    short numPets = 5;
+    int numGrains = 5.6;
+    String name = "Scruffy";
+    numPets.length();
+    numGrains.length();
+    name.length();
+    }
+
+    class Frage4{
+    public class Snake{
+        public void shed(boolean time){
+            if (time) {
+
+            }
+            System.out.println(result);
+        }
+    }
+    }
+    class Frage5{
+    
+    }
