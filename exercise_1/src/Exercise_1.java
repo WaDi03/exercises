cc7828beae4567096a82e635ebf9cd23ac1946ce
wahid riazi <wriazi@terrestris.de>
2023-07-25 11:33:35 +0200

    Exercises from Study Guide

--- exercise_1/src/Exercise_1.java
+++ exercise_1/src/Exercise_1.java
@@ -1,40 +1,54 @@
-    class Frage1{
-        String A$B;
-        String _helloworld;
-        String  true;
-        String java.lang;
-        String Public;
-        String 1980_s;
-    }
-    class Frage2{
+/*class Frage1 {
+    String A$B;
+    String _helloworld;
+    String  true;
+    String java.lang;
+    String Public;
+    String 1980_s;
+}
+
+class Frage2 {
     private String brand;
     private boolean empty;
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Frage2 wb = new Frage2();
-        System.out.println("Empty 0" +wb.empty);
-        System.out.println(", Brand = " +wb.brand);
+        System.out.println("Empty 0" + wb.empty);
+        System.out.println(", Brand = " + wb.brand);
     }
-    }
-    class Frage3{
+}
+
+class Frage3 {
     short numPets = 5;
     int numGrains = 5.6;
     String name = "Scruffy";
     numPets.length();
     numGrains.length();
     name.length();
-    }
+}
 
-    class Frage4{
-    public class Snake{
-        public void shed(boolean time){
+class Frage4 {
+    public class Snake {
+        public void shed(boolean time) {
             if (time) {
 
             }
             System.out.println(result);
         }
     }
+}*/
+
+
+class Frage5 {
+    public static void main(String[] args) {
+        int x = 20;
+
+        while (x > 0) {
+            do {
+                x-=2;
+            } while (x > 5);
+            x--;
+            System.out.println(x + "\t");
+        }
     }
-    class Frage5{
-    
-    }
+}
